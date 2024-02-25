083b92dd4a9489541929d3f37811f6fa96da2741
Koh Xuan Qi <kohhxuanqi@gmail.com>
2024-02-25 22:47:57 +0800

    Handle form submission

--- backend/index.js
+++ backend/index.js
@@ -1,23 +1,12 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const pwnCheck = require('./models/pwnedChecker.js');
-const path = require('path');
-
-const app = express();
+const express = require('express')
+const app = express()
 
 app.use("/",(req, res) => {
     res.send("Server is now running.")
 })
-// Middleware to parse request bodies
-app.use(bodyParser.urlencoded({ extended: true }));
-
-// Serve the HTML page with the registration form
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
 
 // Handle form submission
-app.post('/register', pwnCheck, (req, res) => {
+app.post('/register', (req, res) => {
     const { username, password } = req.body;
     // Simulate successful registration if both username and password are provided
     if (username && password) {
@@ -29,5 +18,4 @@ app.post('/register', pwnCheck, (req, res) => {
     }
 });
 
-// Start the server
 app.listen(5000, console.log("Server is now started on PORT 5000"));
